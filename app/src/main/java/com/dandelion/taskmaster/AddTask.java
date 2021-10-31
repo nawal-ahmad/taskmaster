1e826b9d08841dbccc0890a295f1eba7661b686c
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-10-31 19:38:01 +0200

    add all activities

---
+++ app/src/main/java/com/dandelion/taskmaster/AddTask.java
@@ -0,0 +1,27 @@
+package com.dandelion.taskmaster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        Button addTask = findViewById(R.id.add);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+
+
+
