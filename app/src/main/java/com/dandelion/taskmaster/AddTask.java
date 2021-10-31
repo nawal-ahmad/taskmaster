b468612890591a56c6f916ef31c6ae03becb2abb
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-11-01 00:23:34 +0200

    total tasks

--- app/src/main/java/com/dandelion/taskmaster/AddTask.java
+++ app/src/main/java/com/dandelion/taskmaster/AddTask.java
@@ -1,10 +1,13 @@
 package com.dandelion.taskmaster;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class AddTask extends AppCompatActivity {
 
@@ -13,10 +16,14 @@ public class AddTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+        TextView textView = findViewById(R.id.textView5);
         Button addTask = findViewById(R.id.add);
         addTask.setOnClickListener(new View.OnClickListener() {
+            int counter = 1 ;
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
+                textView.setText("Total Tasks :"+ counter++);
                 Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_LONG).show();
             }
         });
