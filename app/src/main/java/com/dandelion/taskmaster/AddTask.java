db756d2987b92b7af1c78d879d088f3d1489ae0d
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-11-09 01:19:29 +0200

    AWS

--- app/src/main/java/com/dandelion/taskmaster/AddTask.java
+++ app/src/main/java/com/dandelion/taskmaster/AddTask.java
@@ -1,64 +1,64 @@
 package com.dandelion.taskmaster;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
+import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
 
 public class AddTask extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    AppDatabase appDatabase;
+    TaskDao taskDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
-        //////////////////////////////////// save to database ///////////////////////////////
-        // get all edit text data
         EditText taskTitle = findViewById(R.id.taskTitle);
         EditText taskBody = findViewById(R.id.taskBody);
         EditText taskState = findViewById(R.id.taskState);
-        // get add task button
+
+        appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tasks").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        taskDao=appDatabase.taskDao();
+
         Button addTaskBtn = findViewById(R.id.addData);
-        // add listener
         addTaskBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                AppDatabase appDatabase;
                 String title = taskTitle.getText().toString();
                 String body = taskBody.getText().toString();
                 String state = taskState.getText().toString();
 
-                appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tasks").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+                ///////////////////////////////////////////////32//////////////////////////////////////////
+                Task task = Task.builder()
+                        .title(title)
+                        .body(body)
+                        .state(state)
+                        .build();
 
-                // save input fields into object
-                Task task = new Task(title,body,state);
-                // save to db
-                appDatabase.taskDao().insertAll(task);
-                // redirect to menu page
+                // mutations are used to create, update, or delete data
+                Amplify.API.mutate(
+                        ModelMutation.create(task),
+                        response -> Log.i("TasksApp", "add task with id" + response.getData().getId()),
+                        error -> Log.e("TasksApp", "Create failed", error)
+                );
+
                 Intent goToHomePage = new Intent(AddTask.this, MainActivity.class);
                 startActivity(goToHomePage);
             }
         });
-
-
     }
-
-//        TextView textView = findViewById(R.id.textView5);
-//        Button addTask = findViewById(R.id.add);
-//        addTask.setOnClickListener(new View.OnClickListener() {
-//            int counter = 1 ;
-//            @SuppressLint("SetTextI18n")
-//            @Override
-//            public void onClick(View v) {
-//                textView.setText("Total Tasks :"+ counter++);
-//                Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_LONG).show();
-//            }
-//        });
-//    }
 }
 
 
